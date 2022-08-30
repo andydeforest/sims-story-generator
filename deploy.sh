bd842407d3746aea52d3c245791f4a3a8a94486e
Andrew De Forest <andydeforest@gmail.com>
2022-08-30 09:38:45 -0700

    added deployment workflow

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:andydeforest/sims-story-generator.git main:gh-pages
+git push -f git@github.com:andydeforest/sims-story-generator.git deployment:gh-pages
 
 cd -
